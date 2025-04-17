a33d9ccf4ef2887af3d774a0040217ddaea95fc2
Morteza Ansarinia <ansarinia@me.com>
2025-04-17 09:56:20 +0200

    add preprocessing for banana montage

---
+++ src/otka_prepreocessing.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+def convert_signals(signals, Rawdata):
+    signal_names = {
+        k: v
+        for (k, v) in zip(
+            Rawdata.info["ch_names"], list(range(len(Rawdata.info["ch_names"])))
+        )
+    }
+    new_signals = np.vstack(
+        (
+            signals[signal_names["EEG FP1-REF"]]
+            - signals[signal_names["EEG F7-REF"]],  # 0
+            (
+                signals[signal_names["EEG F7-REF"]]
+                - signals[signal_names["EEG T3-REF"]]
+            ),  # 1
+            (
+                signals[signal_names["EEG T3-REF"]]
+                - signals[signal_names["EEG T5-REF"]]
+            ),  # 2
+            (
+                signals[signal_names["EEG T5-REF"]]
+                - signals[signal_names["EEG O1-REF"]]
+            ),  # 3
+            (
+                signals[signal_names["EEG FP2-REF"]]
+                - signals[signal_names["EEG F8-REF"]]
+            ),  # 4
+            (
+                signals[signal_names["EEG F8-REF"]]
+                - signals[signal_names["EEG T4-REF"]]
+            ),  # 5
+            (
+                signals[signal_names["EEG T4-REF"]]
+                - signals[signal_names["EEG T6-REF"]]
+            ),  # 6
+            (
+                signals[signal_names["EEG T6-REF"]]
+                - signals[signal_names["EEG O2-REF"]]
+            ),  # 7
+            (
+                signals[signal_names["EEG FP1-REF"]]
+                - signals[signal_names["EEG F3-REF"]]
+            ),  # 14
+            (
+                signals[signal_names["EEG F3-REF"]]
+                - signals[signal_names["EEG C3-REF"]]
+            ),  # 15
+            (
+                signals[signal_names["EEG C3-REF"]]
+                - signals[signal_names["EEG P3-REF"]]
+            ),  # 16
+            (
+                signals[signal_names["EEG P3-REF"]]
+                - signals[signal_names["EEG O1-REF"]]
+            ),  # 17
+            (
+                signals[signal_names["EEG FP2-REF"]]
+                - signals[signal_names["EEG F4-REF"]]
+            ),  # 18
+            (
+                signals[signal_names["EEG F4-REF"]]
+                - signals[signal_names["EEG C4-REF"]]
+            ),  # 19
+            (
+                signals[signal_names["EEG C4-REF"]]
+                - signals[signal_names["EEG P4-REF"]]
+            ),  # 20
+            (signals[signal_names["EEG P4-REF"]] - signals[signal_names["EEG O2-REF"]]),
+        )
+    )  # 21
+    return new_signals
